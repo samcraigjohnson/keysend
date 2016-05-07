3cdeee61d75e6b147089fb8ea29e55acce45c648
Sam Johnson <sjohnson540@gmail.com>
2016-05-06 22:20:31 -0600

    Add command line arguments and design document

--- keysend.py
+++ keysend.py
@@ -2,10 +2,12 @@ from slacker import Slacker
 
 import os
 import sys
-import tarfile
-from subprocess import call
+import argparse
+import fileinput
+from zipfile import ZipFile
+from subprocess import run, PIPE
 
-CHANNEL = "#_test"
+DEFAULT_CHANNEL = "#general"
 
 slack = Slacker(os.environ['SLACK_API_TOKEN'])
 
@@ -15,35 +17,102 @@ def remove(filename):
     except FileNotFoundError as e:
         pass
 
-def compress(filename, files, compression="xz"):
-    mode = "x:" + compression
-    with tarfile.open(filename, mode=mode) as tar:
+
+def compress(archive, files):
+    """ Change compression method to zip and use -j command to
+    junk the file paths """
+    with ZipFile(archive, mode="x") as z:
         for f in files:
-            tar.add(f)
+            z.write(f, arcname=os.path.basename(f))
+    return archive
+
+#################
+# Keybase stuff #
+#################
+
+def encrypt_message(keybase_user, text):
+    process = run(["keybase", "encrypt", keybase_user, "-m", text],
+                   stdout=PIPE)
+    return process.stdout
 
 def encrypt_file(keybase_user, in_file, out_file):
-    call(["keybase", "encrypt", keybase_user, "-i", in_file, "-o", out_file])
+    run(["keybase", "encrypt", keybase_user, "-i", in_file, "-o", out_file])
 
-def upload_file(filename, channel=CHANNEL):
+
+################
+# Slack stuff  #
+################
+
+def send_message_to_user(message, username):
+    room_id = get_direct_message_id(username)
+    slack.chat.post_message(
+        room_id,
+        message,
+        as_user=True
+    )
+
+
+def send_file_to_user(filename, username):
+    room_id = get_direct_message_id(username)
+    upload_file_to_channel(filename, room_id)
+
+
+def upload_file_to_channel(filename, channel):
     slack.files.upload(filename, channels=channel)
 
 
+def get_direct_message_id(username):
+    user_id = get_user_id(username)
+    return get_room_id(user_id)
+
+
+def get_user_id(username):
+    """ TODO: check to see if user exists """
+    user = [u for u in slack.users.list().body['members']
+            if u['name'] == username.lower()][0]
+    return user['id']
+
+
+def get_room_id(user_id):
+    im = [im for im in slack.im.list().body['ims'] if im['user'] == user_id][0]
+    return im['id']
+
+
 if __name__ == "__main__":
-    # Read the command line arguments
-    to_user = sys.argv[1]
-    in_files = sys.argv[2:]
+    # Handle command line arguments
+    parser = argparse.ArgumentParser(
+        description="Easily send encrypted files through slack"
+    )
+    group = parser.add_mutually_exclusive_group()
 
-    # Compress all the files passed in
-    compressed_file = "/tmp/message.tar.xz"
-    compress(compressed_file, in_files)
+    # Figure out where to send the file
+    group.add_argument("-C", "--channel", help="channel to send file to")
+    group.add_argument("-u", "--user", help="user to send file to")
 
-    # Encrypt the compressed archive
-    out_file = compressed_file + ".enc"
-    encrypt_file(to_user, compressed_file, out_file)
+    # Get extra info
+    parser.add_argument("keybase_user", help="keybase username for encryption")
+    parser.add_argument("-f", "--filename", help="specify output filename")
 
-    # Send encrypted, compressed file to slack channel
-    upload_file(out_file)
+    args, files = parser.parse_known_args()
+    if len(files) == 0:
+        raise "No filenames found"
 
-    # Cleanup files
-    remove(compressed_file)
+    # Filename
+    out_file = "message.zip.enc"
+    if args.filename:
+        out_file = args.filename
+
+    # Channel
+    channel = DEFAULT_CHANNEL
+    if args.channel:
+        channel = args.channel
+    elif args.user:
+        channel = get_direct_message_id(args.user)
+
+    compress_name = "message.zip"
+    compress(compress_name, files)
+    encrypt_file(args.keybase_user, compress_name, out_file)
+    upload_file_to_channel(out_file, channel)
+
+    remove(compress_name)
     remove(out_file)
